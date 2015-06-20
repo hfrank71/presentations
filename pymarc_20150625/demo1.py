ae327a2bb3e50a75bfd7e41bd60c489466b92632
Heidi Frank <hfrank71@gmail.com>
2015-06-20 09:58:03 -0400

    Update demo1.py

--- pymarc_20150625/demo1.py
+++ pymarc_20150625/demo1.py
@@ -1,98 +1,107 @@
-#*~*~*~*~*~*~*~  DRAFT ONLY   DRAFT ONLY   DRAFT ONLY   ~*~*~*~*~*~*~*
-#*~*~*~*~*~*~*~  DEMO Code will be updated closer to the pre-conference date of June 25th   ~*~*~*~*~*~*~*
 import pymarc
 from pymarc import Record, Field
+from copy import deepcopy
 
 ############################################
 # Read in a file of MARC records
 ############################################
-my_marc_recs_in = pymarc.MARCReader(file('my_marc_recs_in_1.mrc'), to_unicode=True, force_utf8=True) 
+my_records_in = pymarc.MARCReader(file('my_marc_recs_in_100r.mrc'), to_unicode=True, force_utf8=True)
+
+############################################
+# Write out a file of MARC records
+############################################
+my_records_out = pymarc.MARCWriter(file('my_marc_recs_out.mrc', 'w'))
 
 
 ############################################
 # Iterate through the input file of MARC records
 ############################################
-for my_rec in my_marc_recs_in:
-	
-	##########################
-	# Get fields out of the record using record.get_fields() function
-	
-	my_245s = my_rec.get_fields('245')	# there should only be *one* field in the list
-#	print my_245s
-	
-	my_245 = my_245s[0]		# specify the list index position zero [0] to return the 1st field in the list
-#	print my_245
-	
-	# OR
-	
-#	print my_rec['245']	# automatically only prints the first occurrence of the specified field
+rec_num = 1
+for my_record in my_records_in:		# iterate through each of the records in the file
+	print 'Record #: '+str(rec_num)
+	my_orig_record = deepcopy(my_record)
+	print_record = False
 	
 	
-	# If there is more than one field in the returned list...
-	my_650s = my_rec.get_fields('650')
-	
-	# iterate through the field objects in the list
-	for my_650 in my_650s:
-#		print my_650['a']
-#		print my_650
-		pass
-		
-		# QUESTION: how to comment out the print command in a for loop if there isn't any other code there?
-		#       that's why I put the do='stuff' line
-	
-#	print my_rec['650']		# automatically only prints the first occurrence of the specified field
+	############################################
+	# DEMO 1 - Get 650 fields from MARC records using record.get_fields() function
+# 	my_650s = my_record.get_fields('650')
+# 	print 'List of field objects returned: '
+# 	print my_650s
+# 	
+# 	print 'Individual fields: '
+# 	for my_650 in my_650s:		# iterate through each of the 650 fields in the record
+# 		print my_650
+# 	print '----------------------------------------------'
 	
 	
-	##########################
-	# Get subfields out of the field using field.get_subfields() function
-#	my_650_subs = my_650.get_subfields()
-	my_650_subs = my_650.get_subfields('a', 'x')
-#	print my_650_subs
+	############################################
+	# DEMO 2 - Add a 710 field to MARC records using record.add_ordered_field() function
+# 	my_new_710 = Field(tag='710', indicators=['2',' '], subfields=['a', 'New York University.', 'b', 'Libraries.'])		# create the new field object
+# 	my_record.add_ordered_field(my_new_710)			# add the new field object to the record
+# 	print_record = True
 	
-	for my_650_sub in my_650_subs:
-#		print my_650_sub
-		pass
+	
+	############################################
+	# DEMO 3 - Analyze 65X fields, check for subfield $2, and modify MARC records	
+# 	my_65Xs = my_record.get_fields('650', '651', '655')
+# 	for my_65X in my_65Xs:		# iterate through each of the 65X fields in the record
+# 		my_65X_2s = my_65X.get_subfields('2')
+# 		if len(my_65X_2s) > 0:				# check if the 65X fields have a subfield $2
+# 			my_record.remove_field(my_65X)
+# 			print 'DELETED FIELD: ' + str(my_65X)
+# 	print '----------------------------------------------'
+	
+	
+	############################################
+	#		HAND-ON EXERCISES
+	############################################
+	
+	# EXERCISE 1 - Extract 1XX fields and print to the screen
+	
+	
+	
+	
+	
+	# EXERCISE 2 - Check if the record contains a 502 thesis note
+	#	If it does, then add a 655 genre field for "Academic theses."
+	
+	
+	
+	
+	
 
-#	print 'Before: '+ str(my_650)
-
-#	my_650.delete_subfield('z')
 	
-#	my_650z_subs = my_650.get_subfields('z')
-#	print my_650
-#	for my_650z in my_650z_subs:
-#		my_650.delete_subfield('z')
-#		print my_650
+	############################################
+	#		HAND-ON EXERCISES - ANSWERS
+	############################################
 	
-#	my_650.add_subfield('2','local')
+	# EXERCISE 1 - Extract 1XX fields and print to the screen
+# 	my_1XXs = my_record.get_fields('100', '110', '111', '130')
+# 	for my_1XX in my_1XXs:
+# 		print my_1XX
+# 	print '----------------------------------------------'
 	
-#	print 'After:  '+ str(my_650)
 	
-	my_new_700 = Field(tag='700', indicators=['1',' '])
-	print my_new_700
+	# EXERCISE 2 - Check if the record contains a 502 thesis note
+	#	If it does, then add a 655 genre field for "Academic theses."
+# 	my_502s = my_record.get_fields('502')
+# 	if len(my_502s) > 0:
+# 		my_new_655 = Field(tag='655', indicators=[' ', '0'], subfields=['a', 'Academic theses.'])
+# 		my_record.add_ordered_field(my_new_655)
+# 		print_record = True
 	
-	print my_new_700.indicator1
-	my_new_700.indicator1 = '2'
-	print my_new_700.indicator1
-	print my_new_700
-	my_new_700.add_subfield('a', 'Melville, Herman,')
-	print my_new_700
-	my_new_700.add_subfield('d', '1819-1891.')
-	print my_new_700
+	
+	if print_record:
+		print '******* Record BEFORE changes *******'
+		print my_orig_record
+		print '******* Record AFTER changes *******'
+		print my_record
+		print '----------------------------------------------'
+	
+	my_records_out.write(my_record)
+	rec_num += 1
 
-	print '----------------------'
-
-
-
-# PUTTING IT ALL TOGETHER
-# 
-# for my_record in my_marc_in:
-# 	rec_245s = my_record.get_fields(‘245’)
-# 	for rec_245 in rec_245s:
-# 		rec_245.add_subfield(‘h’, ‘[electronic resource]’)
-# 		rec_245a_subs = rec_245.get_subfields(‘a’)
-# 		for rec_245a in rec_245a_subs:
-# 			print rec_245a
-# 	my_record.remove_field(‘006’)
-# 	my_new_710 = Field(tag=‘710’, indicators=[‘1’, ‘ ’])
-# 	my_new_710.add_subfield(‘a’, ‘Frank, Heidi’)
-# 	my_record.add_field(my_new_710)
+print 'Total records: ' + str(rec_num-1)
+my_records_in.close()
+my_records_out.close()
